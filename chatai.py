4872167ac83ac8669a6a00f3a0b84d0f34407f4e
Diego Cambiaso <diegocambiaso@gmail.com>
2023-04-10 15:26:27 -0500

    Update chatai.py
    Improved error detection

--- chatai.py
+++ chatai.py
@@ -14,27 +14,33 @@ This script is for educational purposes and will let you run a simple Chat GPT i
 import openai
 
 openai.api_key = (
-    "##-aK9dnLrB3k6RhmJBXA1hT3BlbkFJ5BhA0bHE1jiA1wJHeYtk"  # please, change the API KEY (*)
+    "XX-XXPfEU2qNPP5s3kPOoyxT3BlbkFJdU7r7mfV0KX39ZQ9KCXX"  # please, change the API KEY (*)
 )
 
 print("\nWelcome to Chat AI")
 print("This chat is OpenAI ChatGPT based")
 
 while True:
-    question = input("\nWrite a question: ")
-
-    if question.upper() == "EXIT":
-        print("Good bye")
-        exit()
-
     try:
+        question = input("\nWrite a question: ")
+
+        if question.upper() == "EXIT":
+            print("Good bye")
+            exit()
+
         completion = openai.Completion.create(
             engine="text-davinci-003", prompt=question, max_tokens=2048
         )
 
         print(completion.choices[0].text)  # type: ignore
 
-    except:
+    except openai.error.RateLimitError as APIError:
+        print(APIError)
+        print("Rate limit exced")
+    except FileNotFoundError:
         print("Error during input data or OpenAI API is not available at this moment")
         print('Please try again or write "Exit"')
         continue
+    except KeyboardInterrupt:
+        print("\n\nFinished by the user - Good bye!")
+        exit()
